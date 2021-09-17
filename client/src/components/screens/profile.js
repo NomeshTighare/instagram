fd90906bbcec2e1f01543b95c7892ecfd7d52383
NomeshTighare <Nomeshtighare@gmail.com>
2021-09-18 01:45:18 +0530

    hh

--- client/src/components/screens/profile.js
+++ client/src/components/screens/profile.js
@@ -1,7 +1,46 @@
-import Recat from 'react'
-
+import React,{useEffect,useState,useContext} from 'react'
+import{UserContext} from '../../App'
 
 const Profile = ()=>{
+    const [mypics,setPics] = useState([]);
+    const {state,dispatch} = useContext(UserContext);
+    const [image,setImage] = useState("");
+    useEffect(()=>{
+       fetch('/mypost',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           setPics(result.mypost)
+       })
+    },[]);
+    useEffect(()=>{
+       if(image){
+        const data = new FormData()
+        data.append("file",image)
+        data.append("upload_preset","insta-clone")
+        data.append("cloud_name","nomesh")
+        fetch("https://api.cloudinary.com/v1_1/nomesh/image/upload",{
+            method:"post",
+            body:data
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            localStorage.setItem("user",JSON.stringify({...state,pic:data.url}))
+            dispatch({type:"UPDATEPIC",payload:data.pic})
+            // window.location.reload
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+       }
+    },[image])
+    const updatePhoto = (file)=>{
+        setImage(file);
+    }
     return (
         <div style={{maxWidth:"550px",margin:"0px auto"}}>
             <div style={{
@@ -10,34 +49,50 @@ const Profile = ()=>{
                 margin:"18px 0px",
                 borderBottom:"1px solid grey"
             }}>
+                <div style={{
+               display:"flex",
+               justifyContent:"space-around",
+              
+           }}>
                 <div>
                     <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
-                    src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"
+                    src={state?state.pic:"loading"}
                     />
                 </div>
                 <div>
-                    <h4 style={{fontWeight:"bold"}}>Aniket Vairagade</h4>
-                    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
-                        <h6 style={{fontWeight:"bold"}}>82 Posts</h6>
-                        <h6 style={{fontWeight:"bold"}}>681 Followers</h6>
-                        <h6 style={{fontWeight:"bold"}}>611 Following</h6>
+                <h4>{state?state.name:"loading"}</h4>
+                   <h5>{state?state.email:"loading"}</h5>
+                   <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+                       <h6>{mypics.length} posts</h6>
+                      
+                       <h6>16 followers</h6>
+                       <h6>19 following</h6>
                     </div>
                 </div>
             </div>
-            <div className="gallery">
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
-                <img className="item" src="https://www.roadtocode.org/static/media/aniket.c9b587ed.jpg"/>
             </div>
-            
-      <div style={{margin:"50px", color:"grey", textAlign:"center"}}>
-        <p> © 2021 INSTAGRAM FROM FACEBOOK </p>
-      </div>
+            <div className="file-field input-field" style={{margin:"10px"}}>
+            <div className="btn #64b5f6 blue darken-1">
+                <span>Update pic</span>
+                <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+            </div>
+            </div>
+            <div className="gallery">
+                {
+                    mypics.map(item=>{
+                        return(
+                            <img key={item._id} className="item" src={item.photo} alt={item.title}/>
+                        )
+                    })
+                }
+                
+            </div>
         </div>    
     )
+
 }
 
-export default Profile
+export default Profile;
